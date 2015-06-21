65edd545a5443b731b958b1e012a9f1deed1116c
sergvod <sergvod@yahoo.es>
2015-06-21 22:53:29 +0100

    First complete try

--- cachematrix.R
+++ cachematrix.R
@@ -5,7 +5,17 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
-    
+    inverse <- NULL
+    set <- function(y) {
+        x <<- y
+        inverse <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(cacheSolve) inverse <<- cacheSolve
+    getinverse <- function() inverse
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 }
 
 
